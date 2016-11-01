08eef1a90a39f781a8e9c69318fe831ebaaa6db9
travishuff <huff.travis@gmail.com>
2016-11-01 15:21:05 -0700

    request object proxy set up in express, server test file added

---
+++ testServer.js
@@ -0,0 +1,79 @@
+const express = require('./index.js');
+let app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+app.get('/', 
+function firstFunc(req, res, next) {
+  // res.cookie('secret', (Math.floor(Math.random() * 99)));
+  req.id = `cheng`;
+  // console.log('first middleware');
+  next();
+}, 
+(req, res, next) => {
+  // res.cookie('secret', (Math.floor(Math.random() * 99)));
+  req.id = `travis`;
+  // console.log('second middleware');
+  next();
+}, 
+(req, res, next) => {
+  // res.cookie('secret', (Math.floor(Math.random() * 99)));
+  req.id = `pat`;
+  req.testing = 'testing object';
+  // console.log('third middleware');
+  next();
+}, 
+(req, res, next) => {
+  // res.cookie('secret', (Math.floor(Math.random() * 99)));
+  req.id = `sahil`;
+  // console.log('fourth middleware');
+  next();
+}, 
+(req, res, next) => {
+  // console.log('Cookies: ', req.cookies);
+  // console.log(req.id);
+  req.id = `travis`;
+  // console.log('final middleware');
+  // console.log(req.id);
+  res.redirect('/login');
+}
+);
+
+app.get('/login', 
+function firstFunc(req, res, next) {
+  req.id = `cheng`;
+  // console.log('first middleware');
+  next();
+}, 
+(req, res, next) => {
+  req.id = `travis`;
+  // console.log('second middleware');
+  next();
+}, 
+(req, res, next) => {
+  req.id = `pat`;
+  req.testing = 'testing object';
+  // console.log('third middleware');
+  next();
+}, 
+(req, res, next) => {
+  req.id = `sahil`;
+  // console.log('fourth middleware');
+  next();
+}, 
+(req, res, next) => {
+  // console.log(req.id);
+  req.id = `travis`;
+  // console.log('final middleware');
+  // console.log(req.id);
+  res.end();
+}
+);
+
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
