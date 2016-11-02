5c35e29dd45dbf490da96eb8a9bfefb80f695978
travishuff <huff.travis@gmail.com>
2016-11-02 10:12:25 -0700

    Trying out different data structures for globalMonoStore

---
+++ globalJSON.js
@@ -0,0 +1,152 @@
+module.exports = json = [
+  {
+    "changes": {
+      "0": [
+        {
+          "route": {
+            "path": "/",
+            "stack": [
+              {
+                "name": "firstFunc",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              }
+            ],
+            "methods": {
+              "get": true
+            }
+          }
+        }
+      ],
+      "1": [
+        {
+          "id": "cheng"
+        }
+      ],
+      "2": [
+        {
+          "id": "travis"
+        }
+      ],
+      "3": [
+        {
+          "id": "pat"
+        },
+        {
+          "testing": "testing object"
+        }
+      ],
+      "4": [
+        {
+          "id": "sahil"
+        }
+      ],
+      "5": [
+        {
+          "id": "travis"
+        }
+      ]
+    },
+    "path": "/"
+  },
+  {
+    "changes": {
+      "0": [
+        {
+          "route": {
+            "path": "/login",
+            "stack": [
+              {
+                "name": "firstFunc",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              },
+              {
+                "name": "<anonymous>",
+                "keys": [],
+                "regexp": {},
+                "method": "get"
+              }
+            ],
+            "methods": {
+              "get": true
+            }
+          }
+        }
+      ],
+      "1": [
+        {
+          "id": "cheng"
+        }
+      ],
+      "2": [
+        {
+          "id": "travis"
+        }
+      ],
+      "3": [
+        {
+          "id": "pat"
+        },
+        {
+          "testing": "testing object"
+        }
+      ],
+      "4": [
+        {
+          "id": "sahil"
+        }
+      ],
+      "5": [
+        {
+          "id": "travis"
+        }
+      ]
+    },
+    "path": "/login"
+  }
+];
