037a41172e3e3efabfa6b6a1efe90ab37b4671db
Cheng Ly <chengsieuly@gmail.com>
2016-11-02 18:37:08 -0700

    Build demo

---
+++ demo/server.js
@@ -0,0 +1,32 @@
+const em = require('..');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = em();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/signup', function checkIfUserExist(req, res, next) {
+  req.user = {
+    username: 'hello',
+    password: 'world'
+  };
+
+  next();
+}, function setCookieId(req, res, next) {
+  res.cookie = { token: '32432kd9a01232' };
+  next();
+}, function (req, res, next) {
+  res.redirect('/home');
+});
+
+app.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.listen(3000, () => console.log('connected on port 3000'));
